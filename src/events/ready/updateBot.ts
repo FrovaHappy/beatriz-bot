a7d00e6847686a778fa1be668ad381ca966347ed
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-10-25 14:44:36 -0300

    ♻️ cambiado la presencia y el cooldown a 5 min

--- src/events/ready/updateBot.ts
+++ src/events/ready/updateBot.ts
@@ -1,10 +1,13 @@
+import { ActivityType } from 'discord.js'
 import type { ClientCustom } from '../../types/main'
 
 export default async function updateBot(client: ClientCustom): Promise<void> {
-  const time = 5 * 60 * 60 * 1000
+  const time = 5 * 60 * 1000
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   setInterval(async () => {
     const guilds = (await client.guilds.fetch()).size
-    client.user?.setActivity(`Dando soporte a ${guilds} servers...`)
+    client.user?.setPresence({
+      activities: [{ name: `Dando soporte a ${guilds} servers 🎉`, type: ActivityType.Custom }]
+    })
   }, time)
 }
