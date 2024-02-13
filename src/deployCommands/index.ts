d0e021c223e2ad4f0056d28e403ef549f28ab205
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2024-02-12 21:43:20 -0300

    solved deploy command
    [x]- solve deploy
    [ ]- document changes
    [ ]- improve error detection

--- src/deployCommands/index.ts
+++ src/deployCommands/index.ts
@@ -1,9 +1,7 @@
-import fs from 'node:fs'
-import path from 'node:path'
-import config from '../config'
 import type { BaseFileCommand } from '../types/BaseFiles'
 import updatePublic from './updatePublic'
 import updatePrivate from './updatePrivate'
+import BuildCollection from '../buildCollection'
 
 async function GetCommands(): Promise<Record<'public' | 'private' | 'owner', string[]>> {
   const commands: Record<BaseFileCommand['scope'], string[]> = {
@@ -11,24 +9,10 @@ async function GetCommands(): Promise<Record<'public' | 'private' | 'owner', str
     private: [],
     public: []
   }
-  const foldersPath = path.join(__dirname, '../commands')
-  const commandFolders = fs.readdirSync(foldersPath)
-
-  for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder)
-    const commandFiles = fs
-      .readdirSync(commandsPath)
-      .filter((file: string) => file.endsWith('.js') || file.endsWith('.ts'))
-    for (const file of commandFiles) {
-      const filePath = `../commands/${folder}/${file}`
-      const command: BaseFileCommand = require(filePath).default
-      if (config.validateCommand(command)) {
-        commands[command.scope] = [command.data.toJSON(), ...(commands[command.scope] ?? [])]
-      } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required ${config.commandKeys} property.`)
-      }
-    }
-  }
+  const collection = await BuildCollection<string, BaseFileCommand>('commands')
+  collection.forEach(command => {
+    commands[command.scope] = [command.data.toJSON(), ...(commands[command.scope] ?? [])]
+  })
   return commands
 }
 export default async function deployCommand(): Promise<void> {
