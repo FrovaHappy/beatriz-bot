49c44ac4766550fcfe5d826a3a3d3ff64de8eada
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2024-02-13 01:23:01 -0300

    add canvas for the welcome command
    deleted jsdom

--- src/commands/tests/index.ts
+++ src/commands/tests/index.ts
@@ -1,18 +1,22 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } from 'discord.js'
+import { ActionRowBuilder, AttachmentBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } from 'discord.js'
 import { CommandsNames } from '../../enums'
 import { BuildCommand } from '../../buildersSchema'
-
+import Canvas from '@napi-rs/canvas'
 const name = CommandsNames.test
 export default BuildCommand({
   cooldown: 60,
   name,
-  scope: 'owner',
+  scope: 'private',
   data: new SlashCommandBuilder().setName(name).setDescription('Replies with Pong!'),
   async execute(interaction) {
     if (!interaction.guildId) return
-    console.log(typeof interaction)
+    const c = Canvas.createCanvas(200, 100)
+    const ctx = c.getContext('2d')
+    ctx.font = '30px Arial'
+    ctx.strokeText('Hello World', 10, 50)
+    const attachment = new AttachmentBuilder(await c.encode('png'), { name: 'profile-image.png' })
     const confirm = new ButtonBuilder().setCustomId('confirm').setLabel('Confirm Ban').setStyle(ButtonStyle.Danger)
     const row = new ActionRowBuilder<ButtonBuilder>().addComponents(confirm)
-    interaction.reply({ content: 'test!', components: [row] })
+    interaction.reply({ content: 'test!', components: [row], files: [attachment] })
   }
 })
