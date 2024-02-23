9bd8ae525f59c3a9ad2414fc4ed6b56a9ab0a8ae
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2024-02-22 22:27:18 -0300

    update db and refactor to createServerDb
    - createServerDb implement support to sever private and owner
    - update permission in Server and add Setting

--- src/shared/createServerDb.ts
+++ src/shared/createServerDb.ts
@@ -1,9 +1,13 @@
+import { PermissionBot } from '@prisma/client'
 import db from '../db'
 
-export default async function createServerDb(serverId: string): Promise<void> {
+export default async function createServerDb(serverId: string, isOwner = false, isPrivate = false): Promise<void> {
+  const permissionsBot: PermissionBot[] = [PermissionBot.public]
+  if (isOwner) permissionsBot.push(PermissionBot.owner)
+  if (isPrivate) permissionsBot.push(PermissionBot.private)
   await db.server.upsert({
     where: { serverId },
-    create: { serverId, permissionBot: 'public' },
-    update: {}
+    create: { serverId, permissionsBot },
+    update: { permissionsBot }
   })
 }
