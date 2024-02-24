5ef9ea9c3bdf6be3412f28f02b90c50e468edff4
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2024-02-24 18:40:18 -0300

    support to sequences add

--- src/commands/shared/messageFormatting.ts
+++ src/commands/shared/messageFormatting.ts
@@ -1,14 +1,25 @@
 const rules = {
-  color: '<color>',
   userName: '<user_name>',
   userGlobal: '<user_global>',
   userCount: '<user_count>',
   userId: '<user_id>'
 }
-export default function messageFormatting(str: string, formats: Partial<typeof rules>): string {
-  const formatsKeys = Object.keys(formats)
+const rulesInternal = {
+  color: '<color>'
+}
+const rulesRequired = {
+  '\\n': '\n',
+  '\\t': '\t',
+  '\\r': '\r'
+}
+export default function messageFormatting(str: string, formats: Partial<typeof rules & typeof rulesInternal>): string {
+  const formatsKeys = Object.keys(formats) as Array<keyof typeof rules & keyof typeof rulesInternal>
   for (const fk of formatsKeys) {
-    str = str.replaceAll(rules[fk as keyof typeof rules], formats[fk as keyof Partial<typeof rules>] ?? '')
+    str = str.replaceAll(rules[fk], formats[fk] ?? '')
+  }
+  const rulesReqKeys = Object.keys(rulesRequired) as Array<keyof typeof rulesRequired>
+  for (const rrk of rulesReqKeys) {
+    str = str.replaceAll(rrk, rulesRequired[rrk])
   }
   return str
 }
