780eafb3aea250263adaa8e4566efe82f1c38d60
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-04-06 23:59:28 -0300

    apply render canvas

--- src/shared/buildWelcomeImage/index.ts
+++ src/shared/buildWelcomeImage/index.ts
@@ -1,35 +1,34 @@
-import { GlobalFonts, createCanvas } from '@napi-rs/canvas'
+import { GlobalFonts, createCanvas, Path2D as Path, loadImage } from '@napi-rs/canvas'
 import { type Canvas } from '../../commands/setWelcome/validate'
-import iconPencil from './iconPencil'
 import { AttachmentBuilder, type GuildMember } from 'discord.js'
-import textPencil from './textPincel'
-import imagePencil from './imagePencil'
 
 // load Fonts
 import fonts from '../fonts'
+import renderCanvas from '../renderCanvas'
+import type { Base, Layer, TextBase } from '@/types/Canvas.types'
 for (const font of fonts) {
   GlobalFonts.registerFromPath(font.patch, font.nameAlias)
 }
 
 export default async function buildWelcomeImage(data: Canvas, member: GuildMember): Promise<AttachmentBuilder> {
-  const canvas = createCanvas(data.width, data.height)
-  let ctx = canvas.getContext('2d')
-  ctx.fillStyle = data.background ?? 'transparent'
-  ctx.fillRect(0, 0, data.width, data.height)
-  for (const layer of data.layers) {
-    if (layer.type === 'name' || layer.type === 'text') {
-      ctx = await textPencil({ ctx, member, text: layer as any, colorDominate: data.colorDominate ?? 'black' })
-    }
-    if (layer.type === 'icon') {
-      const cLayer = await iconPencil({ member, icon: layer as any })
-      ctx.drawImage(cLayer, layer.x, layer.y)
-    }
-    if (layer.type === 'image') {
-      const cLayer = await imagePencil(layer as any)
-      ctx.drawImage(cLayer, layer.x, layer.y)
-    }
+  const user = {
+    avatar: member.avatarURL(),
+    count: member.guild.memberCount,
+    globalName: member.user.globalName,
+    id: member.user.id,
+    username: member.user.username
   }
-  ctx.scale(1.5, 1.5)
+  const { layers, ...base } = data
+  const canvas = createCanvas(data.width, data.height)
+  const ctx = canvas.getContext('2d')
+  await renderCanvas(
+    layers as unknown as Layer[],
+    base as unknown as Base & TextBase,
+    user,
+    ctx as unknown as CanvasRenderingContext2D,
+    Path as unknown as typeof Path2D,
+    loadImage as unknown as (path: string) => Promise<HTMLImageElement>
+  )
   const extension = 'png'
 
   return new AttachmentBuilder(await canvas.encode(extension), { name: `welcome.${extension}` })
